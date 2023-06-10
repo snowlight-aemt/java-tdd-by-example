6575da45a8b50b058ed1ad921d7ce956a20c43ac
snowlight-aemt <it.s.aemt@gmail.com>
2023-06-10 19:29:45 +0900

    Dollar 부작용

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 * $5 + 10CHF = $10
 * ~~$5 * 2 = $10~~ 
 * amount 를 private 으로 만들기
-* Dollar 부작용
+* ~~Dollar 부작용~~
 * Money 반올림
--- src/test/java/me/snowlight/DollarTest.java
+++ src/test/java/me/snowlight/DollarTest.java
@@ -7,13 +7,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class DollarTest {
 
-    @DisplayName("Dollar 곱셈 | $5 * 2 = $10")
+    @DisplayName("Dollar 곱셈, Dollar 불변성 | $5 * 2 = $10")
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
+        Dollar product = five.times(2);
+        assertThat(product.amount).isEqualTo(10);
 
-        assertThat(five.amount).isEqualTo(10);
+        product = five.times(3);
+        assertThat(product.amount).isEqualTo(15);
     }
 
     private class Dollar {
@@ -23,8 +25,8 @@ class DollarTest {
             this.amount = amount;
         }
 
-        public void times(int multiplier) {
-            this.amount *= multiplier;
+        public Dollar times(int multiplier) {
+            return new Dollar(this.amount * multiplier);
         }
     }
 }
