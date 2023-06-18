f186f8275403776421ffa4b8446746af6aeca2df
snowlight-aemt <it.s.aemt@gmail.com>
2023-06-18 09:22:10 +0900

    정리 | 간단한 테스트 기능 구현

--- README.md
+++ README.md
@@ -5,11 +5,11 @@
 * ~~$5 * 2 = $10~~ 
 * ~~amount 를 private 으로 만들기~~
 * ~~Dollar 부작용~~
-* Money 반올림
+* ~~Money 반올림~~
 * ~~equals()~~
-* hashCode()
-* equals null
-* equals object (Dollar 가 아닌 객체)
+* ~~hashCode()~~
+* ~~equals null~~
+* ~~equals object (Dollar 가 아닌 객체)~~
 * ~~5CHF * 2 = 10CHF~~
   * $5 + 10CHF = $10 | 큰 테스트 처리를 위한 작은 테스트
   * 테스트 성공 확인 까지만 진행. 추가 리팩토링 진행.
@@ -18,4 +18,4 @@
 * ~~공용 times~~
 * ~~Franc 과 Dollar 비교하기~~
 * ~~통화 (화폐 단위)~~
-* testFrancMultiplication 를 제거 할까?
+* ~~testFrancMultiplication 를 제거 할까?~~
--- src/test/java/me/snowlight/Money.java
+++ src/test/java/me/snowlight/Money.java
@@ -1,5 +1,7 @@
 package me.snowlight;
 
+import java.util.Objects;
+
 class Money {
     protected int amount;
     protected String currency;
@@ -26,9 +28,17 @@ class Money {
     }
 
     @Override
-    public boolean equals(Object obj) {
-        Money money = (Money) obj;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Money money = (Money) o;
         return this.amount == money.amount
                 && this.currency.equals(money.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, currency);
     }
 }
