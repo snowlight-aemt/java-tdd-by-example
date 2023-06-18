96ee95f6accca1dd92d32564a45923a41a711b7f
snowlight-aemt <it.s.aemt@gmail.com>
2023-06-18 09:11:00 +0900

    하위 클래스를 참조 하는 테스트 코드(부분) 제거 | 하위 클래스 제거 | ch11

--- src/test/java/me/snowlight/Dollar.java
+++
@@ -1,7 +0,0 @@
-package me.snowlight;
-
-class Dollar extends Money {
-    public Dollar(int amount, String currency) {
-        super(amount, currency);
-    }
-}
--- src/test/java/me/snowlight/Franc.java
+++
@@ -1,7 +0,0 @@
-package me.snowlight;
-
-class Franc extends Money {
-    public Franc(int amount, String currency) {
-        super(amount, currency);
-    }
-}
